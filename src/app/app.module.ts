c3b681ecdc7f558997ba492ef3754404137420c0
Cristina <cristina.m.pedro@gmail.com>
2023-01-16 09:39:02 +0000

    video page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ChannelSuggestionsComponent } from './channel-suggestions/channel-sugge
 import { ThemeSuggestionsComponent } from './theme-suggestions/theme-suggestions.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {HttpClientModule} from "@angular/common/http";
+import { ChannelListComponent } from './channel-list/channel-list.component';
+import { CardChannelListComponent } from './card-channel-list/card-channel-list.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import {HttpClientModule} from "@angular/common/http";
     HomepageComponent,
     ChannelPageComponent,
     ChannelSuggestionsComponent,
-    ThemeSuggestionsComponent
+    ThemeSuggestionsComponent,
+    ChannelListComponent,
+    CardChannelListComponent
   ],
   imports: [
     BrowserModule,
