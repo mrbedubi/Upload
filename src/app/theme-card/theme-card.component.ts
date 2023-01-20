1c7733062bce3a72711189f48062c7607b348fad
tomás <tomas_lx_99@live.com.pt>
2023-01-20 17:04:56 +0000

    um pouco de css na article-page; novo componente da lista de tematicas

---
+++ src/app/theme-card/theme-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-card',
+  templateUrl: './theme-card.component.html',
+  styleUrls: ['./theme-card.component.scss']
+})
+export class ThemeCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
