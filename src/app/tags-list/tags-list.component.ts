45098e55030819c381544fa843a93c1f26b64e66
Mr.Bedubi <diogo.azev97@gmail.com>
2023-01-23 11:30:45 +0000

    popUp tags + search by tags

--- src/app/tags-list/tags-list.component.ts
+++ src/app/tags-list/tags-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {NavbarComponent} from "../navbar/navbar.component";
+import {UploadService} from "../upload.service";
+import {Tag} from "../interface";
+import * as events from "events";
+import {query} from "@angular/animations";
 
 @Component({
   selector: 'app-tags-list',
@@ -7,10 +11,57 @@ import {NavbarComponent} from "../navbar/navbar.component";
   styleUrls: ['./tags-list.component.scss']
 })
 export class TagsListComponent implements OnInit {
+tagsSelected: number[]=[];
+tags: Tag[]=[];
+tagQuery:string="";
 
-  constructor( public nav:NavbarComponent) { }
+
+
+  constructor( public nav:NavbarComponent , private service:UploadService) { }
 
   ngOnInit(): void {
+
+    this.service.getTags().subscribe((tag)=>{
+      this.tags=tag;
+    })
+
+
+  }
+
+  select(id:number){
+
+  if(this.isSelected(id)){
+    this.tagsSelected=this.tagsSelected.filter( tag => tag!== id)
+  }
+  else {
+    this.tagsSelected.push(id);
+  }
+    console.log(this.tagsSelected);
+  }
+
+
+  isSelected(id:number){
+    const isSelected= this.tagsSelected.includes(id);
+    return isSelected
+  }
+
+
+  cleanSearchTags(){
+    this.tagsSelected=[];
+  }
+
+  searchTags(){
+    let s="";
+    this.tagsSelected.forEach(tag=>{
+     this.tagQuery+=tag+",";
+    });
+    this.tagQuery=this.tagQuery.slice(0,this.tagQuery.length-1);
+    s=this.tagQuery;
+    this.tagQuery="";
+    console.log(s);
+    return s;
   }
 
 }
+
+
